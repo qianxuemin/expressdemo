dff32e044f80036e9148f183db6cbcab83e5b812
qianxuemin <710728787@qq.com>
2017-09-03 12:40:52 +0800

    连接数据库

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+//加载第三方模块
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+
 //配置swig
 swig = require('swig');
 app.set('view engine', 'html');
@@ -7,23 +10,56 @@ app.engine('html', swig.renderFile);
 
 app.use(express.static('public'));
 
+//设置mysql
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'myexpress'
+});
+
+connection.connect();
+
 //配置路由
 app.get('/', function (req, res,next) {
+    console.log('准备渲染');
+    next();
+  },function (req, res,next) {
   //res.send('Hello World!');
+  //后台向前台传数据
   res.render('index',{
-  	title:"首页",
-  	data:"提交"
+    title:"首页",
+    data:"提交"
   });
 });
 
 app.get('/receive',function(req, res,next){
-  var uname=req.query.username
-  console.log(req.query)
-  res.json({
-   code:123,
-   defaultuser:'qianxuemin',
-   username:uname
-  })
+  var uname=req.query.username;
+  // 无数据库时模拟直接返回数据
+  // res.json({
+  //  code:123,
+  //  defaultuser:'qianxuemin',
+  //  username:uname
+  // });
+
+//写到数据库
+  var post  = {username:uname};
+  var query = connection.query('INSERT INTO userinfo SET ?', post, function (error, results, fields) {
+    if (error) {
+      res.json({
+       success:'no',
+       defaultuser:'qianxuemin',
+       msg:'username插入失败'
+      });
+    }else{
+      res.json({
+       success:'ok',
+       defaultuser:'qianxuemin',
+       msg:'username插入成功'
+      });
+    }
+    
+  });
 })
 
 /*容错机制*/
--- readme.txt
+++ readme.txt
@@ -7,7 +7,11 @@ npm install --save swig  //模板引擎
 
 模板必须放在views文件夹下
 
+npm install --save mysql
+
+
+
 执行npm install 后
 node app.js可进行测试 
 
-express中间件  next()  类似踩一个个小石头过河 串联起来达到终点
+express核心 中间件  next()  类似踩一个个小石头过河 串联起来达到终点
